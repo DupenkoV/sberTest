dcba1170abf0245969c0442c745fee6f71ccb560
Dupenko Vladimir <v.s.dupenko@gmail.com>
2023-10-10 14:30:10 +0300

    таск 1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,22 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js';
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const excluded = ['groupId', 'service', 'formatSize', 'ca'];
 
+const uniqKeys = new Set();
 
+const decoded = encoded.map(item => {
+  const itemKeys = Object.keys(item);
+  itemKeys.forEach(key => {
+    if (key.includes('Id') && !excluded.includes(key)) {
+      const value = item[key];
+      item[key] = translations[value];
+      if (!translations.hasOwnProperty(value)) {
+        uniqKeys.add(key);
+      }
+    }
+  });
+  return item;
+});
 
-// console.log(decoded)
+console.log(decoded);
+console.log(Array.from(uniqKeys));
