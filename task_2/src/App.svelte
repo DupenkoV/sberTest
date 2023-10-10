2d310fe062033df2bf1da9fe586db48f3d75627e
Dupenko Vladimir <v.s.dupenko@gmail.com>
2023-10-10 21:24:23 +0300

    недоделанна логика пересчета валют

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,61 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let currenciesList:string[] = [];
+  let conversion_rates:any = {};
+
+  let first_currencie: string = 'RUB'
+  let second_currencie: string = 'USD'
+  let first_currencie_input: number = 0;
+  let second_currencie_input: number = 0;
+
+  function calc(type: string) {
+    if(type === 'first') {
+      return conversion_rates[first_currencie] > conversion_rates[second_currencie] ?  first_currencie_input * conversion_rates[second_currencie] : conversion_rates[first_currencie] / first_currencie_input
+    } else  {
+      return conversion_rates[first_currencie] < conversion_rates[second_currencie] ?  second_currencie_input * conversion_rates[first_currencie] : conversion_rates[second_currencie] / second_currencie_input
+    }
+  }
+  function changeValue(event) {
+    if(event.target.getAttribute('name') === 'first_currencie') {
+      console.log(1)
+      first_currencie_input = event.target.value;
+      second_currencie_input = calc('first')
+    } else {
+      second_currencie_input = event.target.value;
+      first_currencie_input = calc('second')
+    }
+  }
+  $: {
+    console.log(conversion_rates)
+  }
+  function fetchConversion(item:string) {
+    fetch(`https://v6.exchangerate-api.com/v6/1e2df0d944881b36269ef760/latest/${item}`).then(res => res.json()).then(data => {
+    conversion_rates = data.conversion_rates;
+    currenciesList = Object.keys(data.conversion_rates);
+  })
+  }
+  $: {
+    fetchConversion(first_currencie);
+  }
+
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <select name="first_currencie" id="first_currencie" bind:value={first_currencie} >
+    $: {#each currenciesList as currencies}
+    <option value={currencies} >{currencies}</option>
+    {/each}
+  </select>
+  <input type="number" name="first_currencie" value={first_currencie_input} min={0} on:input={changeValue}>
+  =
+  <select name="second_currencie" id="second_currencie" bind:value={second_currencie}>
+    $: {#each currenciesList as currencies}
+    <option value={currencies}>{currencies}</option>
+    {/each}
+  </select>
+  <input type="number" name="second_currencie" value={second_currencie_input} min={0} on:input={changeValue}>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
