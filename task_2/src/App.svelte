571e7c80bc11b7e17a429afe3e7de8d71c87200b
Dupenko Vladimir <v.s.dupenko@gmail.com>
2023-10-10 23:19:46 +0300

    завершена логика перевода согласно курсу валют, добавлены комментарии к коду

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,59 +1,68 @@
 <script lang="ts">
   let currenciesList:string[] = [];
-  let conversion_rates:any = {};
+  let conversion_rates:any = [];
 
-  let first_currencie: string = 'RUB'
-  let second_currencie: string = 'USD'
-  let first_currencie_input: number = 0;
-  let second_currencie_input: number = 0;
+  let firstCurrencie: string = 'RUB'
+  let secondCurrencie: string = 'USD'
+  let firstCurrencieInput: number = 0;
+  let secondCurrencieInput: number = 0;
 
+  //Функция подсчета суммы обмена валюты
   function calc(type: string) {
     if(type === 'first') {
-      return conversion_rates[first_currencie] > conversion_rates[second_currencie] ?  first_currencie_input * conversion_rates[second_currencie] : conversion_rates[first_currencie] / first_currencie_input
+      return Math.ceil((firstCurrencieInput * conversion_rates[secondCurrencie])*100)/100
     } else  {
-      return conversion_rates[first_currencie] < conversion_rates[second_currencie] ?  second_currencie_input * conversion_rates[first_currencie] : conversion_rates[second_currencie] / second_currencie_input
+      return Math.ceil((secondCurrencieInput / conversion_rates[secondCurrencie])*100)/100
     }
   }
-  function changeValue(event) {
+
+  //Функция очистки инпутов для избежания незапланированного поведения.
+  function clearInput() {
+    firstCurrencieInput = 0;
+    secondCurrencieInput = 0;
+  }
+
+  //Функция изменения значения инпутов.
+  function changeValue(event: any) {
     if(event.target.getAttribute('name') === 'first_currencie') {
-      console.log(1)
-      first_currencie_input = event.target.value;
-      second_currencie_input = calc('first')
+      firstCurrencieInput = event.target.value;
+      secondCurrencieInput = calc('first')
     } else {
-      second_currencie_input = event.target.value;
-      first_currencie_input = calc('second')
+      secondCurrencieInput = event.target.value;
+      firstCurrencieInput = calc('second')
     }
-  }
-  $: {
-    console.log(conversion_rates)
-  }
+  }  
+
+
+  //Функция запроса на сервер для получения объекта валют
   function fetchConversion(item:string) {
     fetch(`https://v6.exchangerate-api.com/v6/1e2df0d944881b36269ef760/latest/${item}`).then(res => res.json()).then(data => {
     conversion_rates = data.conversion_rates;
     currenciesList = Object.keys(data.conversion_rates);
   })
   }
+
   $: {
-    fetchConversion(first_currencie);
+    fetchConversion(firstCurrencie);
   }
 
 
 </script>
 
 <main>
-  <select name="first_currencie" id="first_currencie" bind:value={first_currencie} >
+  <select name="first_currencie" id="first_currencie" bind:value={firstCurrencie} on:change={clearInput}>
     $: {#each currenciesList as currencies}
     <option value={currencies} >{currencies}</option>
     {/each}
   </select>
-  <input type="number" name="first_currencie" value={first_currencie_input} min={0} on:input={changeValue}>
+  <input type="number" name="first_currencie" value={firstCurrencieInput} min={0} on:input={changeValue}>
   =
-  <select name="second_currencie" id="second_currencie" bind:value={second_currencie}>
+  <select name="second_currencie" id="second_currencie" bind:value={secondCurrencie} on:change={clearInput}>
     $: {#each currenciesList as currencies}
     <option value={currencies}>{currencies}</option>
     {/each}
   </select>
-  <input type="number" name="second_currencie" value={second_currencie_input} min={0} on:input={changeValue}>
+  <input type="number" name="second_currencie" value={secondCurrencieInput} min={0} on:input={changeValue}>
 </main>
 
 <style>
